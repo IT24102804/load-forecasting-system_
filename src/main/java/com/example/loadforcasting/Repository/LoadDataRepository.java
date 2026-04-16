3baacd701277ebe4bd8eb001b7f03c3fc0ca0834
IT24102804 <it24102804@my.sliit.lk>
2026-04-16 17:43:20 +0530

    feat(load): update load data repository

--- src/main/java/com/example/loadforcasting/Repository/LoadDataRepository.java
+++ src/main/java/com/example/loadforcasting/Repository/LoadDataRepository.java
@@ -5,11 +5,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.transaction.annotation.Transactional;
+import java.time.LocalDateTime;
 
 public interface LoadDataRepository extends JpaRepository<LoadData, Integer> {
 
     @Modifying
     @Transactional
     @Query("DELETE FROM LoadData l WHERE l.timestamp < ?1")
-    void deleteOldData(String cutoffDate);
+    void deleteOldData(LocalDateTime cutoffDate);
 }
